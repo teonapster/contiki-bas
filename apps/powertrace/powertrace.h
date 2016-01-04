b6e8ad7daa531a1fb6c9322f84878b82caad1d01
Kouroutzidis Theodoros <teonapster@gmail.com>
2016-01-04 16:27:25 +0200

    Automate energy consumption analysis (gnuplot & Makefile)

--- apps/powertrace/powertrace.h
+++ apps/powertrace/powertrace.h
@@ -45,13 +45,5 @@
 void powertrace_start(clock_time_t perioc);
 void powertrace_stop(void);
 
-typedef enum {
-  POWERTRACE_ON,
-  POWERTRACE_OFF
-} powertrace_onoff_t;
-
-void powertrace_sniff(powertrace_onoff_t onoff);
-
 void powertrace_print(char *str);
-
 #endif /* POWERTRACE_H */
